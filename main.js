0b3dd2580c39ffe0a01ec012bcd764aa271c2196
Alvaro Obyrne <alvarobyrne@gmail.com>
2025-12-23 19:21:44 -0500

    Refactor block dimensions and positions for clarity and consistency

--- main.js
+++ main.js
@@ -8,22 +8,51 @@ import { addVertices } from "./vertices.js";
 
 console.log("Hello, World!", Math.random());
 
-const tansversalBlockSize = 1;
+const transversalBlockSize = 1;
 
-const dimension1 = tansversalBlockSize * 5;
-
+const dimension1 = transversalBlockSize * 5;
+const dimension2 = transversalBlockSize * 4;
+const dimension3 = transversalBlockSize * 5;
 // Cube dimensions
 const width = dimension1,
-  height = tansversalBlockSize,
-  depth = tansversalBlockSize;
+  height = transversalBlockSize,
+  depth = transversalBlockSize;
 const block1Dimensions = {
   width: dimension1,
-  height: tansversalBlockSize,
-  depth: tansversalBlockSize,
+  height: transversalBlockSize,
+  depth: transversalBlockSize,
 };
+const block2Dimensions = {
+  width: transversalBlockSize,
+  height: dimension2,
+  depth: transversalBlockSize,
+};
+const block3Dimensions = {
+  width: transversalBlockSize,
+  height: dimension3,
+  depth: transversalBlockSize,
+};
+
 const { scene, renderer, camera } = setupScene();
+
 const block1 = createBlock(block1Dimensions);
+const block2 = createBlock(block2Dimensions);
+const block3 = createBlock(block3Dimensions);
+
 scene.add(block1);
+scene.add(block2);
+scene.add(block3);
+
+const isAddingGaps = true;
+const gapSize = isAddingGaps ? 0.05 : 0;
+
+block2.position.x = dimension1 / 2 - transversalBlockSize / 2 ; // small offset to avoid z-fighting
+block2.position.y = dimension2 / 2 + transversalBlockSize / 2 + gapSize; // small offset to avoid z-fighting
+block2.position.z = 0;
+
+block3.position.x = -dimension1 / 2 + transversalBlockSize / 2 ; // small offset to avoid z-fighting
+block3.position.y = dimension3 / 2 + transversalBlockSize / 2 + gapSize; // small offset to avoid z-fighting
+block3.position.z = 0;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
