1da6d9d090dde68cc1e9fae476502747aa4142b9
Alvaro Obyrne <alvarobyrne@gmail.com>
2026-01-16 22:40:14 -0500

    Add packages for markdown convertion
    in order to make the readme file also the instructions available at the production site

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
 import { defineConfig } from 'vite';
+import { marked } from 'marked';
 
 export default defineConfig({
     base: 'https://alvarobyrne.github.io/infinite-cube/',
+    plugins: [
+        {
+            name: 'markdown-html',
+            async transform(code, id) {
+                if (/\.(md)$/.test(id)) {
+                    // convert to html with `marked`
+                    const html = await marked(code);
+                    return {
+                        // return the .js file we would want to import
+                        code: `
+                            export const html = ${JSON.stringify(html)};
+                            export const md = ${JSON.stringify(code)};
+                        `,
+                        map: null,
+                    };
+                }
+            },
+        },
+    ],
 });
